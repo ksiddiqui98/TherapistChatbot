325f54be11387b91753a6bd0dcf5e88d7a19edb0
Kinza Siddiqui <ksiddiqui1998@gmail.com>
2018-12-10 21:12:10 -0500

    nlu in assistant

--- assistant.py
+++ assistant.py
@@ -1,5 +1,14 @@
-from watson_developer_cloud import AssistantV2
+from watson_developer_cloud import AssistantV2, NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+    import Features, EmotionOptions, SentimentOptions
 import time
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2018-03-16',
+    username='63a8e859-3e1a-49b9-87e9-49da3f47c68e',
+    password='oQWwNPacKhRD',
+    url='https://gateway.watsonplatform.net/natural-language-understanding/api'
+)
 
 assistant = AssistantV2(
     version='2018-11-08',
@@ -7,7 +16,7 @@ assistant = AssistantV2(
     password='ttZyjLhxQpGt',
     url='https://gateway.watsonplatform.net/assistant/api'
 )
-assistant_id = 'fc12d1d1-4027-4295-ad7a-a0ac1bba7418'
+assistant_id = '83f6f620-47c4-40a2-a002-f59644add9b9'
 
 session_id = assistant.create_session(
     assistant_id = assistant_id
@@ -43,3 +52,20 @@ assistant.delete_session(
     assistant_id= assistant_id,
     session_id= session_id
 )
+
+#get emotions and sentiment score from text
+def getFeatures():
+    response = natural_language_understanding.analyze(
+        text="I have way too many exams this week and I'm stressed out",
+        features=Features(sentiment=SentimentOptions(), emotion = EmotionOptions())
+    ).get_result()
+
+    sentimentScore = response['sentiment']['document']['score']
+    allEmotions = response['emotion']['document']['emotion']
+    emotions = {}
+    for e in allEmotions:
+        if allEmotions[e] > .3:
+            emotions = {e: allEmotions[e]}
+
+    print("Sentiment Score: ", sentimentScore)
+    print("Emotions: ", emotions)
